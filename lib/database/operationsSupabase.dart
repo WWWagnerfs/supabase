7ce1f0922247e00f55cf91f0979a20e604d22624
wagner <wwwagnerfs@gmail.com>
2024-03-20 02:16:33 -0300

    Operações básicas

--- lib/database/operationsSupabase.dart
+++ lib/database/operationsSupabase.dart
@@ -3,11 +3,11 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 class OperationsSupabaseDB{
   final supabase = Supabase.instance.client;
 
-  Future<void> insertRowSupabase(String nome, String email, String telefone) async {
+  Future<void> insertRowSupabase(String nome, String email, String cpf, String dtn, String telefone,) async {
 
     await supabase
         .from('cadastro')
-        .insert({'nome': nome, 'email': email, 'telefone': telefone});
+        .insert({'nome': nome, 'email': email, 'cpf': cpf, 'data de nascimento':dtn, 'telefone': telefone});
   }
 }
 
